c1f907f7d3e3ad8c1a86f8512e5a88ef66535417
SHeFF33 <134718156+SHeFF33@users.noreply.github.com>
2024-01-16 10:44:30 +0300

    Создание проекта
    Был создан интерфейс для будущей команды

---
+++ WinFormsApp123/WinFormsApp123/Form1.cs
@@ -0,0 +1,18 @@
+
+namespace WinFormsApp123
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
